589f4cc9be7756897af0209634435f59011aa21d
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <xux@microsoft.com>
2018-09-27 16:57:05 +0800

    Add project files.

---
+++ TestMultiViews/js/main.js
@@ -0,0 +1,14 @@
+﻿// Your code here!
+document.querySelector("#open").addEventListener("click", function () {
+    var targetWindow = window.open("/extWindow.html", null, "msHideView=yes"),
+        viewId = MSApp.getViewId(targetWindow),
+        ViewManagement = Windows.UI.ViewManagement,
+        ApplicationView = ViewManagement.ApplicationView.getForCurrentView();
+
+    ApplicationView.addEventListener("consolidated", function () {
+        targetWindow.close();
+    });
+
+    ViewManagement.ApplicationViewSwitcher.tryShowAsStandaloneAsync(viewId,
+        ViewManagement.ViewSizePreference.default, ApplicationView.id, ViewManagement.ViewSizePreference.default);
+});
